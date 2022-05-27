383e026efd785551e5dfb77ebbeef039f61d1a71
Bhavya Singh <Bhavya.Singh@ibm.com>
2022-05-27 15:28:19 +0530

    Removing unused code

--- service/src/test/java/net/apmoller/crb/ohm/microservices/producer/library/utils/ResponseStubs.java
+++ service/src/test/java/net/apmoller/crb/ohm/microservices/producer/library/utils/ResponseStubs.java
@@ -1,16 +1,9 @@
 package net.apmoller.crb.ohm.microservices.producer.library.utils;
 
-import net.apmoller.crb.ohm.microservices.producer.library.models.User;
-
 public class ResponseStubs {
 
     public static final String NAME = "Komal";
     public static final String DEPT = "Maersk";
 
-    public static User createUser() {
-        User user = User.builder().build();
-        user.setName(NAME);
-        user.setDept(DEPT);
-        return user;
-    }
+
 }
